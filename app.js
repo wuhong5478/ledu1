e9cbecfb82615cfb3380b8676c4463433c826d75
wb.wuhong <wb.wuhong@mesg.corp.netease.com>
2016-09-19 14:09:39 +0800

    add static

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+
+app.use(express.static(path.join(__dirname, 'static')));
 app.get('/',function(req,res){
    res.sendFile(path.join(__dirname+'/index.html'));
 });
