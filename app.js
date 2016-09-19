7115c2b319a76c075f2980ddda5d55255536ae35
wb.wuhong <wb.wuhong@mesg.corp.netease.com>
2016-09-19 13:44:33 +0800

    no message

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+app.get('/',function(req,res){
+   res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.listen(80);
+console.log("Express server listening on port %d in %s mode", 80, app.settings.env);
