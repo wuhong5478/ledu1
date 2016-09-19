b577a8428dcba72bd39d92dcb3b8c4a7bbd94429
wb.wuhong <wb.wuhong@mesg.corp.netease.com>
2016-09-19 15:36:35 +0800

    no message

--- public/static/js/run.js
+++
@@ -1,1 +0,0 @@
-if(!window.JSON){var height=document.documentElement.clientHeight - 101;if(document.getElementById('needHeight').offsetHeight<height){document.getElementById('needHeight').style.height=height+'px';}}
