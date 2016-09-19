5dfb96be1968155871d6badac86c16a4e898a66d
wb.wuhong <wb.wuhong@mesg.corp.netease.com>
2016-09-19 14:05:00 +0800

    add ledu

---
+++ static/js/run.js
@@ -0,0 +1,1 @@
+if(!window.JSON){var height=document.documentElement.clientHeight - 101;if(document.getElementById('needHeight').offsetHeight<height){document.getElementById('needHeight').style.height=height+'px';}}
